2902a01be63ff993c7246301952db4ee1cef0396
fwcd <fwcd@fwcd.dev>
2024-05-21 02:36:00 +0200

    Replace Text -> NSText

--- WorldWideWeb/HyperText.h
+++ WorldWideWeb/HyperText.h
@@ -17,7 +17,7 @@
 
 extern void write_rtf_header(NSStream* rtfStream);
 
-@interface HyperText:Text
+@interface HyperText:NSText
 {
 	id 	server;		//	Responsible for maintaining this node
 //	NSMutableArray *	Anchors;	//	A list of the anchors 
