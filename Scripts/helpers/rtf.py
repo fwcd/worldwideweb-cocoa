f8bfc214dd86183ed9c255ca1d6fec1ecd00acd6
fwcd <fwcd@fwcd.dev>
2024-09-14 20:53:14 +0200

    Fix import

--- Scripts/helpers/rtf.py
+++ Scripts/helpers/rtf.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Optional, Self
 
-from Scripts.helpers.reader import Reader
+from helpers.reader import Reader
 
 # Based on https://www.biblioscape.com/rtf15_spec.htm
 
