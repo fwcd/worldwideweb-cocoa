4d0472ccdbc9be15f0082aa9142a17658a0ada03
fwcd <fwcd@fwcd.dev>
2024-09-14 20:25:52 +0200

    Factor out generic reader

---
+++ Scripts/helpers/parse.py
@@ -0,0 +1,54 @@
+from typing import Generic, Self, TypeVar
+
+class ParseError(Exception):
+    pass
+
+T = TypeVar('T')
+
+class Reader(Generic[T]):
+    def __init__(self, raw_str: T, i: int = 0):
+        self.raw = raw_str
+        self.i = i
+    
+    def child(self) -> Self:
+        return Reader(self.raw, self.i)
+
+    def peek(self, n: int = 1) -> T:
+        if self.i + n > len(self.raw):
+            raise ParseError(f'RTF file ended too early, could not read next {n} char(s)')
+
+        return self.raw[self.i:self.i + n]
+
+    def skip(self, n: int = 1):
+        self.i += n
+
+    def next(self, n: int = 1) -> T:
+        s = self.peek(n)
+        self.skip(n)
+        return s
+
+    def expect(self, expected: T):
+        s = self.next(len(expected))
+        if s != expected:
+            raise ParseError(f"Expected '{expected}', but got '{s}'")
+    
+    def read_word(self) -> str:
+        assert isinstance(self.raw, str), 'read_word is only implemented for string readers'
+
+        w = ''
+        while (c := self.peek()) and c.isalpha():
+            w += c
+            self.skip()
+        return w
+
+    def read_int(self) -> int:
+        assert isinstance(self.raw, str), 'read_int is only implemented for string readers'
+
+        d = ''
+        if (c := self.peek()) and c == '-':
+            d += c
+            self.skip()
+        while (c := self.peek()) and c.isnumeric():
+            d += c
+            self.skip()
+        return int(d)
--- Scripts/helpers/rtf.py
+++ Scripts/helpers/rtf.py
@@ -1,54 +1,11 @@
 from dataclasses import dataclass, field
 from typing import Optional, Self
 
+from helpers.parse import Reader
+
 # Based on https://www.biblioscape.com/rtf15_spec.htm
 
-class RTFParseError(Exception):
-    pass
-
-class RTFReader:
-    def __init__(self, raw_str: str, i: int = 0):
-        self.raw_str = raw_str
-        self.i = i
-    
-    def child(self) -> Self:
-        return RTFReader(self.raw_str, self.i)
-
-    def peek(self, n: int = 1) -> str:
-        if self.i + n > len(self.raw_str):
-            raise RTFParseError(f'RTF file ended too early, could not read next {n} char(s)')
-
-        return self.raw_str[self.i:self.i + n]
-
-    def skip(self, n: int = 1):
-        self.i += n
-
-    def next(self, n: int = 1) -> str:
-        s = self.peek(n)
-        self.skip(n)
-        return s
-
-    def expect(self, expected: str):
-        s = self.next(len(expected))
-        if s != expected:
-            raise RTFParseError(f"Expected '{expected}', but got '{s}'")
-    
-    def read_word(self) -> str:
-        w = ''
-        while (c := self.peek()) and c.isalpha():
-            w += c
-            self.skip()
-        return w
-
-    def read_int(self) -> int:
-        d = ''
-        if (c := self.peek()) and c == '-':
-            d += c
-            self.skip()
-        while (c := self.peek()) and c.isnumeric():
-            d += c
-            self.skip()
-        return int(d)
+# Lower level (unstructured) parsing
 
 @dataclass
 class RTFControlWord:
@@ -56,7 +13,7 @@ class RTFControlWord:
     value: Optional[int] = None
 
     @classmethod
-    def parse_from(cls, r: RTFReader) -> Self:
+    def parse_from(cls, r: Reader[str]) -> Self:
         r.expect('\\')
         name = r.read_word()
         if r.peek().isnumeric():
@@ -73,7 +30,7 @@ class RTFGroup:
     elements: list['RTFNode'] = field(default_factory=list)
 
     @classmethod
-    def parse_from(cls, r: RTFReader) -> Self:
+    def parse_from(cls, r: Reader[str]) -> Self:
         elements = []
         r.expect('{')
         while r.peek() != '}':
@@ -81,10 +38,6 @@ class RTFGroup:
         r.skip()
         return cls(elements)
 
-    @classmethod
-    def parse(cls, s: str) -> Self:
-        return cls.parse_from(RTFReader(s))
-    
     def __str__(self) -> str:
         return f"{{{''.join(map(str, self.elements))}}}"
 
@@ -93,7 +46,7 @@ class RTFText:
     value: str
 
     @classmethod
-    def parse_from(cls, r: RTFReader) -> Self:
+    def parse_from(cls, r: Reader[str]) -> Self:
         value = ''
         while (c := r.peek()) and c not in {'\\', '{', '}'}:
             value += c
@@ -108,7 +61,7 @@ class RTFNode:
     value: RTFControlWord | RTFGroup | RTFText
 
     @classmethod
-    def parse_from(cls, r: RTFReader) -> Self:
+    def parse_from(cls, r: Reader[str]) -> Self:
         match r.peek():
             case '\\': return cls(RTFControlWord.parse_from(r))
             case '{': return cls(RTFGroup.parse_from(r))
