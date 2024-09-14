b4320acc03187d16c17f780d2a62c9fda14471d3
fwcd <fwcd@fwcd.dev>
2024-09-14 03:08:08 +0200

    Move print info handling into app delegate and use NSApplicationMain

--- WorldWideWeb/AppDelegate.m
+++ WorldWideWeb/AppDelegate.m
@@ -7,6 +7,12 @@
 
 #import "AppDelegate.h"
 
+#import <AppKit/AppKit.h>
+#import <Foundation/Foundation.h>
+#import <libc.h> /* TBL */
+#import <stdlib.h>
+#import <string.h> /* TBL */
+
 @interface AppDelegate ()
 
 @property(strong) IBOutlet NSWindow *window;
@@ -16,6 +22,34 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    NSDictionary<NSString *, id> *myDefaults = @{
+        @"PaperType" : @"Letter", // Non-USA users will have to override
+        @"LeftMargin" : @"72",    //  (72) Space for ring binding
+        @"RightMargin" : @"36",   //  (72) Note printers need some margin
+        @"TopMargin" : @"36",     // (108) All margins in points
+        @"BottomMargin" : @"36",  // (108) PrintInfo defaults in brackets
+    };
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults registerDefaults:myDefaults];
+
+    //    The default margins seem to be 72, 72, 108, 108 which is a lot.
+    {
+        NSInteger leftM = [defaults integerForKey:@"LeftMargin"];
+        NSInteger rightM = [defaults integerForKey:@"RightMargin"];
+        NSInteger topM = [defaults integerForKey:@"TopMargin"];
+        NSInteger bottomM = [defaults integerForKey:@"BottomMargin"];
+
+        NSPrintInfo *pi = [NSPrintInfo sharedPrintInfo];
+        // TOOD: Is paper name the equivalent of paper type?
+        [pi setPaperName:[[NSUserDefaults standardUserDefaults] stringForKey:@"PaperType"]];
+        [pi setVerticallyCentered:NO];
+        [pi setLeftMargin:leftM];
+        [pi setRightMargin:rightM];
+        [pi setTopMargin:topM];
+        [pi setBottomMargin:bottomM];
+    }
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
--- WorldWideWeb/WorldWideWeb_main.m
+++ WorldWideWeb/WorldWideWeb_main.m
@@ -15,23 +15,10 @@
 
 extern char *appDirectory; /* Name of the directory containing the application */
 
-static int int_default(NSString *param) { return (int)[[NSUserDefaults standardUserDefaults] integerForKey:param]; }
-
-int main(int argc, char *argv[]) {
-    //    NXArgc = argc;		/* TBL */
-    //    NXArgv = argv;		/* TBL */
-
-    NSApplication *app = [NSApplication sharedApplication];
-
+int main(int argc, const char *argv[]) {
+    // Cocoa port note: Some of the logic been moved to the AppDelegate, so we can use NSApplicationMain
+    
     char *p;
-
-    NSDictionary<NSString *, id> *myDefaults = @{
-        @"PaperType" : @"Letter", // Non-USA users will have to override
-        @"LeftMargin" : @"72",    //  (72) Space for ring binding
-        @"RightMargin" : @"36",   //  (72) Note printers need some margin
-        @"TopMargin" : @"36",     // (108) All margins in points
-        @"BottomMargin" : @"36",  // (108) PrintInfo defaults in brackets
-    };
 
     appDirectory = malloc(strlen(argv[0]));
     strcpy(appDirectory, argv[0]);
@@ -39,29 +26,6 @@ int main(int argc, char *argv[]) {
         p[1] = 0; /* Chop home directory after slash */
     if (TRACE)
         NSLog(@"WWW: Run from %s", appDirectory);
-
-    [[NSUserDefaults standardUserDefaults] registerDefaults:myDefaults];
-
-    NSArray *tl;
-    [[NSBundle mainBundle] loadNibNamed:@"WorldWideWeb.nib" owner:app topLevelObjects:&tl];
-
-    //	The default margins seem to be 72, 72, 108, 108 which is a lot.
-    {
-        int leftM = int_default(@"LeftMargin");
-        int rightM = int_default(@"RightMargin");
-        int topM = int_default(@"TopMargin");
-        int bottomM = int_default(@"BottomMargin");
-
-        NSPrintInfo *pi = [NSPrintInfo sharedPrintInfo];
-        // TOOD: Is paper name the equivalent of paper type?
-        [pi setPaperName:[[NSUserDefaults standardUserDefaults] stringForKey:@"PaperType"]];
-        [pi setVerticallyCentered:NO];
-        [pi setLeftMargin:leftM];
-        [pi setRightMargin:rightM];
-        [pi setTopMargin:topM];
-        [pi setBottomMargin:bottomM];
-    }
-
-    [app run];
-    return 0;
+    
+    return NSApplicationMain(argc, argv);
 }
